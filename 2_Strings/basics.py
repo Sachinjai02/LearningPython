ad07b0cd267280d323dee6d5181f9f321ad014c5
sachinja <sachinja@blueplanet.com>
2023-09-16 17:08:52 +0530

    basics of strings in python - including indexing and slicing

---
+++ 2_Strings/basics.py
@@ -0,0 +1,40 @@
+# h   e   l   l   o
+# 0   1   2   3   4
+# -5 -4  -3  -2  -1
+print('hello')
+print("hello")
+print("This is Sachin Jain's laptop")
+print("hello \nworld")
+print("hello \tworld")
+
+print(len("hello baby!"))
+
+mystring = "Hello World"
+print(mystring)
+
+print(mystring[0])
+
+# negative indexing
+print(mystring[-2])
+print(mystring[9])
+
+#substring from 2nd index till end
+print(mystring[2:])
+
+#substring from 0th till 5th (note 6th idx is not included)
+print(mystring[:6])
+
+#from start (inclusive) to end (not inclusive)
+print(mystring[2:8])
+
+#step size default of 1
+print(mystring[::])
+
+#step size of 2
+print(mystring[::2])
+
+#reverse a String; negative step size
+print(mystring[::-1])
+
+print(mystring[-1:-12:-1])
+
