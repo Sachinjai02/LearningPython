50a4c92324d1614e5fe07a3c3db2417e98af6bb3
sachinja <sachinja@blueplanet.com>
2023-09-16 13:26:19 +0530

    Playing with numbers and variables

---
+++ 1_Numbers/basics.py
@@ -0,0 +1,18 @@
+print(2 + 3)
+print(2 % 3)
+print(1 / 2)
+
+# This is a comment
+myNumber = 124
+myNumber1 = 3455
+print(myNumber)
+print(myNumber1)
+
+my_dogs = 2
+print('type of my_dogs is')
+print(type(my_dogs))
+print(my_dogs)
+my_dogs = ["dog1", "sammy"]
+print('type of my_dogs is')
+print(type(my_dogs))
+print(my_dogs)
