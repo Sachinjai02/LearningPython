c4b074c603644eca43a1da73421f25ef58582f59
sachinja <sachinja@blueplanet.com>
2023-09-20 23:00:21 +0530

    playing with sets

---
+++ 6_Sets/basics.py
@@ -0,0 +1,15 @@
+my_set = set()
+my_set.add(1)
+my_set.add(2)
+
+print(my_set)
+my_set.add(1)
+print(my_set)
+
+my_set = {2,3,4}
+print(my_set)
+
+my_set = set([1,2,2,2,3,4,444,3,3,3,3,4])
+print(my_set)
+
+print(set("Mississippi"))
