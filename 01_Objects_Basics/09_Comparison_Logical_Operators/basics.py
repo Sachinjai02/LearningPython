b35388882ae84fd67b4f39897897b7a000072ec4
sachinja <sachinja@blueplanet.com>
2023-09-24 21:32:49 +0530

    comparison and logical operators

---
+++ 01_Objects_Basics/09_Comparison_Logical_Operators/basics.py
@@ -0,0 +1,19 @@
+print(2=='2')
+print('bye'=='Bye')
+print(2.0==2)
+print(True==False)
+
+print(4 != 3 )
+print(4 > 3)
+print(4>=3)
+print(3 <=3)
+print (3 < 4)
+
+##Logical operators
+print ( 1>0 and 3 == 3)
+print (1<2<3)
+
+print(1 > 4 or 5 == 5.0)
+
+print (not True)
+print (not 1 == 1)
