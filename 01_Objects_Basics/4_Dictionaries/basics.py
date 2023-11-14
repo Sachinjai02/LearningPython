9b01a35a16735541f485d3309a9134b3d26c7013
sachinja <sachinja@blueplanet.com>
2023-11-14 13:38:47 +0530

    playing with dictionaries

---
+++ 01_Objects_Basics/4_Dictionaries/basics.py
@@ -0,0 +1,7 @@
+my_dic = {'key': 2345, 'k2': 12222, 1234: 2334}
+print(my_dic)
+
+print(type(my_dic))
+print(my_dic.values()) # list of values
+print(my_dic.keys()) # list of keys
+print(my_dic.items()) # list of tuples of key/values
